3bc928f73c3120f004fb0b70772a0f42fa8010b6
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-05-25 18:44:42 -0400

    Implement React Router 5

---
+++ app/javascript/react/containers/OpenGamesContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class OpenGamesContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <p>
+        Hello from the Open Games Container
+      </p>
+    )
+  }
+}
+
+export default OpenGamesContainer
