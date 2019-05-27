b80ba4f7da6d763d53738889798fcecc639c78c3
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-05-27 08:20:28 -0400

    Add timestamps to Snakes

--- db/migrate/20190527121521_create_snakes.rb
+++ db/migrate/20190527121521_create_snakes.rb
@@ -2,6 +2,8 @@ class CreateSnakes < ActiveRecord::Migration[5.2]
   def change
     create_table :snakes do |t|
       t.belongs_to :direction, null: false
+
+      t.timestamps
     end
   end
 end
