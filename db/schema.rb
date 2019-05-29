a7d62f1958be29f0a38cd0c6e018f4e59eddf344
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-05-29 12:17:30 -0400

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_153906) do
+ActiveRecord::Schema.define(version: 2019_05_29_161618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_153906) do
     t.string "direction", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["direction"], name: "index_directions_on_direction", unique: true
   end
 
   create_table "games", force: :cascade do |t|
