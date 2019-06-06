16a3d7b88b873f00d62c18f62f932c0a8890d324
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-06-06 18:02:32 -0400

    Add factory for each model

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -7,4 +7,33 @@ FactoryBot.define do
     password_confirmation { 'password' }
   end
 
+  factory :coordinate do
+    column_position { Faker::Number.within(1..50) }
+    row_position { Faker::Number.within(1..50) }
+  end
+
+  factory :direction do
+    direction { ["Left", "Right", "Up", "Down"].sample }
+  end
+
+  factory :game do
+
+  end
+
+  factory :snake do
+    direction { FactoryBot.create(:direction) }
+    user { FactoryBot.create(:user) }
+    game { FactoryBot.create(:game) }
+  end
+
+  factory :snake_coordinate do
+    snake { FactoryBot.create(:snake) }
+    coordinate { FactoryBot.create(:coordinate) }
+    head { [false, true].sample }
+  end
+
+  factory :user_game do
+    user { FactoryBot.create(:user) }
+    game { FactoryBot.create(:game) }
+  end
 end
